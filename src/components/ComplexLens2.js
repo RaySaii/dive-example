19f6acf7674afe9249a87afd2d262c037518ae28
raysaii <309406931@qq.com>
2019-01-22 18:07:35 +0800

    1.删除state里多余判断

--- src/components/ComplexLens2.js
+++ src/components/ComplexLens2.js
@@ -7,7 +7,7 @@ import styles from './styles.module.scss'
 const ComplexLens2 = dive({
   lens: {
     get: state => {
-      return ({ ...state.complex2, hello: state.complex1 ? state.complex1.hello : '' })
+      return ({ ...state.complex2, hello: state.complex1.hello })
     },
     set: (state, ownState) => ({ ...state, complex2: ownState }),
   },
