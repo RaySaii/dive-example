1ba7d6934ce1d1e55a6c7276833b478b8e1b6ffc
wanglei <00000000>
2019-04-11 23:34:01 +0800

    1.修改.gitignore

--- src/components/DealHttp.js
+++ src/components/DealHttp.js
@@ -1,37 +1,42 @@
-import dive, {fromHttp, HttpComponent} from 'divejs'
+import dive, {xhrWithStatus} from 'divejs'
 import {merge, of} from 'rxjs'
-import {map, mapTo, switchMap, debounceTime, filter, distinctUntilChanged} from 'rxjs/operators'
+import {
+  map,
+  mapTo,
+  switchMap,
+  debounceTime,
+  filter,
+  distinctUntilChanged,
+} from 'rxjs/operators'
 import React from 'react'
 import styles from './styles.module.scss'
 
 const DealHttp = dive({
   state: { repos: {} },
 })(({ state$, eventHandle }) => {
-  const fetchRepos = (q) => fromHttp(
-      fetch(`https://api.github.com/search/repositories?q=${q}&sort=stars&order=desc&`)
-          .then(res => res.json()),
-  )
+  const fetchRepos = q => fetch(`https://api.github.com/search/repositories?q=${q}&sort=stars&order=desc&`)
+      .then(res => res.json())
+
   eventHandle.event('change').pipe(
       debounceTime(500),
       distinctUntilChanged(),
-      switchMap(val => val ? fetchRepos(val) : of({})),
-  ).reduce(repos => state => {
-    state.repos = repos
-  })
+      xhrWithStatus(val => val.length > 0 ? fetchRepos(val) : []),
+  )
+      .reduce(([repos, loading]) => state => {
+        state.repos = repos
+        state.loading = loading
+      })
+
   return state$.pipe(
       map(state => {
         return (
             <div className={styles.git_box}>
               <div>git repository:</div>
               <input onChange={e => eventHandle.handle('change')(e.target.value)}/>
-              <HttpComponent
-                  status={state.repos.status}
-                  loading={<div>Loading...</div>}
-                  data={state.repos.data}
-                  render={data => data.items.slice(0, 12).map((item, index) => (
-                      <div key={index}>{item.name}</div>
-                  ))}
-              />
+              {state.loading && <div>Loading...</div>}
+              {(state.repos && state.repos.items) && state.repos.items
+                  .slice(0, 12)
+                  .map((item, index) => <div key={index}>{item.name}</div>)}
             </div>
         )
       }),
